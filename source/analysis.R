d04f5d365127390670b2c2771b8161c0ec8ec891
martinma0104 <chengm26@uw.edu>
2022-02-24 23:19:38 -0800

    A3

--- source/analysis.R
+++ source/analysis.R
@@ -1,1 +1,33 @@
+file <- "https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv"
+county_level <- read.csv(file, header=TRUE)
+View(county_level)
 
+file <- "https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends_jail_jurisdiction.csv"
+jurisdiction_level <- read.csv(file, header=TRUE)
+View(jurisdiction_level)
+
+#5 computed data
+library(dplyr)
+jail_population <- county_level %>%
+  filter(year == max(year, na.rm = TRUE)) %>%
+  pull(total_pop)
+
+barplot(jail_population, main="Jail population", xlab="Population in 2018 in different county",  
+        ylab="Total population",
+        border="blue", density=c(10,20,30,40,50))
+
+Stat_with_most_population <- county_level %>%
+  filter(total_pop == max(total_pop, na.rm=TRUE)) %>%
+  pull(state)
+
+Stat_with_least_population <- county_level %>%
+  filter(total_pop == min(total_pop, na.rm=TRUE)) %>%
+  pull(state)
+
+Year_with_most_population <- county_level %>%
+  filter(total_pop == max(total_pop, na.rm=TRUE)) %>%
+  pull(year)
+
+Year_with_least_population <- county_level %>%
+  filter(total_pop == min(total_pop, na.rm=TRUE)) %>%
+  pull(year)
